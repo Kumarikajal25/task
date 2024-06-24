32d8239bf9521b595100559110be82560d94ca23
Kumarikajal25 <kumarinaina9876@gmail.com>
2024-06-24 20:59:10 +0530

    new

--- app/color/page.tsx
+++ app/color/page.tsx
@@ -31,7 +31,7 @@ const ColorPalette: React.FC = () => {
     };
 
     return (
-        <div className="p-4 min-h-screen">
+        <div className="">
             <h1 className="text-2xl font-bold mb-4">Color Palette</h1>
             <div className="flex space-x-2">
                 {colors.map((color, index) => (
